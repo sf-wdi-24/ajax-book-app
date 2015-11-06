f3a234135b1077821c015781a7615cdb92158275
cameronjacoby <cameron.jacoby@gmail.com>
2015-11-05 19:33:29 -0800

    implement update form in book template

--- main.js
+++ main.js
@@ -1,31 +1,67 @@
 // wait for DOM to load before running JS
 $(function() {
 
-  // form to create new book
-  var $createBook = $('#create-book');
+  // base API route
+  var baseUrl = 'https://super-crud.herokuapp.com/books';
+
+  // array to hold book data from API
+  var allBooks = [];
 
   // element to display list of books
   var $booksList = $('#books-list');
 
-  // loading gif
-  var $loading = $('#loading');
+  // form to create new book
+  var $createBook = $('#create-book');
 
   // compile handlebars template
   var source = $('#books-template').html();
   var template = Handlebars.compile(source);
 
-  // GET all books
-  $.get('https://super-crud.herokuapp.com/books', function (data) {
-    console.log(data);
+  // helper function to render all books to view
+  // note: we empty and re-render the collection each time our book data changes
+  var render = function() {
+    // empty existing books from view
+    $booksList.empty();
 
-    // books are in an array called `books`
-    var allBooks = data.books;
-
-    // pass in data to render in the template
+    // pass `allBooks` into the template function
     var booksHtml = template({ books: allBooks });
 
     // append html to the view
     $booksList.append(booksHtml);
+  };
+
+  // GET all books on page load
+  $.get(baseUrl, function (data) {
+    console.log(data);
+
+    // set `allBooks` to book data from API
+    allBooks = data.books;
+
+    // render all books to view
+    render();
+  });
+
+  // listen for submit even on form
+  $createBook.on('submit', function (event) {
+    event.preventDefault();
+
+    // serialze form data
+    var newBook = $(this).serialize();
+
+    // POST request to create new book
+    $.post(baseUrl, newBook, function (data) {
+      console.log(data);
+
+      // add new book to `allBooks`
+      allBooks.push(data);
+
+      // render all books to view
+      render();
+    });
+
+    // reset the form
+    $createBook[0].reset();
+    $createBook.find('input').first().focus();
   });
 
 });
