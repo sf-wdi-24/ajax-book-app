5f6b176969e02abfcfa8fb93c3b70b816cc1bcfd
stanleyyork <stanleyyork@gmail.com>
2015-11-09 15:25:07 -0800

    added link to goodreads

--- main.js
+++ main.js
@@ -3,7 +3,78 @@ $(function() {
 
   // check to make sure JS is loaded
   console.log('JS is loaded!');
+
+  var source = $('#books-list-template').html(); // loads the html from index.html
+  var template = Handlebars.compile(source); // template is a function
+  $('#new-book-form').hide();
+  $('#completegif').hide();
+  getBooks();
   
   // your code here
+  function getBooks(){
+    $.get('https://super-crud.herokuapp.com/books', function(data){
+    	var books = data.books;
+    	if(books.length > 0){
+    		var trackHtml = template({book: books});
+    		$('#books-list').append(trackHtml);
+    	} else {
+    		$('#books-list').append("No Results Available");
+    	}
+    });
+  }
+
+  function addBook(title, author, imageUrl, releaseDate){
+  	var options = {
+  		"title": title,
+  		"author": author,
+  		"image": imageUrl,
+  		"releaseDate": releaseDate
+  	};
+  	$.ajax({
+	  type: "POST",
+	  url: 'https://super-crud.herokuapp.com/books',
+	  data: options,
+	  success: function(){
+      $('#new-book-form').slideUp("slow");
+      $('#completegif').show();
+      setTimeout(function(){$('#completegif').slideUp("slow")}, 3000);
+      getBooks();
+	  }
+	});
+  }
+
+  $('#new-book-form-submit').click(function(){
+  	var title = $('#title-form').val();
+  	var author = $('#author-form').val();
+  	var imageUrl = $('#image-url-form').val();
+  	var releaseDate = $('#release-date-form').val();
+  	addBook(title, author, imageUrl, releaseDate);
+  });
+
+  $('.glyphicon').mouseenter(function(){
+  	console.log("mouseover");
+  	//$('.glyphicon').css('color', "green");
+  });
+
+  $('.glyphicon').click(function(){
+  	console.log("in here");
+  	$(this).css('color', "green");
+  });
+
+  $('#add-book').mouseenter(function(){
+  	$(this).css('background-color', "#5bc0de");
+  });
+
+  $('#add-book').mouseout(function(){
+  	$(this).css('background-color', "grey");
+  });
+
+  $('#add-book').click(function(){
+  	if ($('#new-book-form').is( ":hidden" )) {
+    	$('#new-book-form').slideDown( "slow" );
+  	} else {
+    	$('#new-book-form').slideUp("slow");
+    }
+  });
 
 });
