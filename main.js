3ee0efa1bc84d9bfebdba5c827093f28e0d2c9f2
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2015-11-09 21:18:49 -0800

    PUT is buggy but made some progress

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 // wait for DOM to load before running JS
 $(document).ready(function(data) {
 
-	$(function() {
+	// check to make sure JS is loaded
+	console.log("JS is loaded!");
 
-		// check to make sure JS is loaded
-		console.log("JS is loaded!");
+	var source = $("#books").html();
+	var template = Handlebars.compile(source);
 
-		var source = $("#books").html();
-		var template = Handlebars.compile(source);
+	// Get a list of all books
+	$('#showbooks').on('click', function(event) {
 
-		// Get a list of all books
 		$.get("https://super-crud.herokuapp.com/books", function(data) {
 			console.log(data);
 
@@ -18,35 +18,67 @@ $(document).ready(function(data) {
 			});
 			$("#bookslist").append(booksHtml);
 		});
-		// Create a new book
+	});
+	// Create a new book
 
-		// Need an HTML for that passes data 
-		$('#book-form').on('submit', function(event) {
-			event.preventDefault();
-			console.log('submitting form!');
-			console.log($(this).serialize());
-
-			var newBook = $(this).serialize();
-
-			$.ajax({
-				type: 'POST',
-				url: 'https://super-crud.herokuapp.com/books',
-				data: newBook,
-				success: function(data) {
-					$('#title').val();
-					$('#author').val();
-				}
-			});
-
-		});
+	// Need an HTML for that passes data 
+	$('#book-form').on('submit', function(event) {
+		event.preventDefault();
+		console.log('submitting form!');
+		console.log($(this).serialize());
 
 		// Need a submit handler
 
 		// Need to read form data (in the handler)
 
 		// Create the book using a post method, with the form values
-	});
 
+		var newBook = $(this).serialize();
+
+		$.ajax({
+			type: 'POST',
+			url: 'https://super-crud.herokuapp.com/books',
+			data: newBook,
+			success: function(data) {
+				$('#title').val();
+				$('#author').val();
+			}
+		});
+
+		// Need another form that shows up in the list of book, with Edit and Delete
+		// The Edit and Delete buttons need to be connected to JS file as "PUT" and "DELETE" functionality
+		// So handlebars should ouput an Edit and Delete button with each book.  
+		// The Edit and Delete buttons are Bootstrap buttons
+
+
+	});
+	$('.book-edit').on('submit', function(event) {
+		event.preventDefault();
+
+		// find the book's id (stored in HTML as `data-id`)
+
+		var bookId = $(this).closest('.book').attr('data-id');
+
+		// find the book to update by its id
+		var bookToUpdate = allBooks.filter(function(book) {
+			return book._id == bookId;
+		})[0];
+		// serialze form data
+
+		$.ajax({
+			type: 'PUT',
+			url: 'https://super-crud.herokuapp.com/books',
+			data: updatedBook,
+			success: function(data) {
+				allBooks.splice(allBooks.indexOf(bookToUpdate), 1, data);
+				console.log("Something was clicked!");
+
+				// render all books to view
+				render();
+			}
+		});
+
+	});
 });
 
 
