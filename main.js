1404b3bde6484288c4d0eba1719adabb42e83b6f
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2015-11-09 17:40:14 -0800

    got create a book to work

--- main.js
+++ main.js
@@ -1,52 +1,56 @@
 // wait for DOM to load before running JS
-$(function() {
+$(document).ready(function(data) {
 
-	// check to make sure JS is loaded
-	console.log("JS is loaded!");
+	$(function() {
 
-	var source = $("#books").html();
-	var template = Handlebars.compile(source);
+		// check to make sure JS is loaded
+		console.log("JS is loaded!");
 
-	$.get("https://super-crud.herokuapp.com/books", function(data) {
-		console.log(data);
+		var source = $("#books").html();
+		var template = Handlebars.compile(source);
 
-		var booksHtml = template({
-			books: data.books
+		// Get a list of all books
+		$.get("https://super-crud.herokuapp.com/books", function(data) {
+			console.log(data);
+
+			var booksHtml = template({
+				books: data.books
+			});
+			$("#bookslist").append(booksHtml);
 		});
-		$("#bookslist").append(booksHtml);
+		// Create a new book
+
+		// Need an HTML for that passes data 
+		$('#book-form').on('submit', function(event) {
+			event.preventDefault();
+			console.log('submitting form!');
+			console.log($(this).serialize());
+
+			var newBook = $(this).serialize();
+
+			$.ajax({
+				type: 'POST',
+				url: 'https://super-crud.herokuapp.com/books',
+				data: newBook,
+				success: function(data) {
+					$('#title').val();
+					$('#author').val();
+				}
+			});
+
+		});
+
+		// Need a submit handler
+
+		// Need to read form data (in the handler)
+
+		// Create the book using a post method, with the form values
 	});
 
 });
 
-$("create-book").on("submit", function(event) {
-			event.preventDefault();
-			console.log($(this.serialize());
 
-				var newBook = $(this).serialize();
-
-				$.ajax({
-					type: "POST",
-					url: "https://super-crud.herokuapp.com/books"
-					data: newBook,
-					success: function(data) {
-
-						console.log(data);
-
-					}
-				});
-
-
-				$.post("https://super-crud.herokuapp.com/books, "
-					newBook,
-					function(data) {
-						booksCollection.push(data);
-						console.log(data)
-					}
-
-				)
-			};
-
-			// 1. form to enter data in HTML
-			// 2. collect data from from in JS
-			// 3. AJAX POST request to server to create new book
-			// 4. handle response from API  - render a new book to the page
+// 1. form to enter data in HTML
+// 2. collect data from from in JS
+// 3. AJAX POST request to server to create new book
+// 4. handle response from API  - render a new book to the page
