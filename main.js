1c1dd203bc527992ef65f939e751ed96c53be13c
stevennoble78 <stevennoble78@hotmail.com>
2015-11-09 17:15:59 -0800

    create added

--- main.js
+++ main.js
@@ -3,7 +3,56 @@ $(function() {
 
   // check to make sure JS is loaded
   console.log('JS is loaded!');
-  
-  // your code here
+
+  // Compile the template
+  var source   = $('#template').html();
+	var template = Handlebars.compile(source);
+	var bookResults = [];
+
+  // Use AJAX to get data and append it to the page
+  $.get("https://super-crud.herokuapp.com/books/", function(data) {
+  	console.log(data.books);
+  	bookResults = data.books;
+
+	 	// Render the data
+	  var bookHTML = template({books: bookResults});
+	  $('#books-list').append(bookHTML);
+  });
+
+	function refresh (data) {
+		$('#books-list').empty();
+		$('input').val('');
+		// Rerender the data
+	  var bookHTML = template({books: bookResults});
+	  $('#books-list').append(bookHTML);
+	}
+
+	function addBook(data) {
+		bookResults.push(data);
+		refresh();
+	}
+
+	function putBook(data) {
+		event.preventDefault();
+		var url = 'https://super-crud.herokuapp.com/books/' + $(this)._id;
+		$.ajax({
+			type: 'PUT',
+			url: url,
+			success: function (data) {
+				bookResults.splice()
+			}
+		});
+	}
+
+  $('#newBook').on('submit', function(event) {
+  	event.preventDefault();
+  	console.log($(this).serialize());
+		var newBook = $(this).serialize();
+		$.post("https://super-crud.herokuapp.com/books/", newBook, addBook);
+  });
+
+  $('.glyphicon-pencil').click(putBook);
+
+
 
 });
